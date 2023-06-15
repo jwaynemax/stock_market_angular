78b277021b9e1409f70995c000b6d23de87cb9b3
jm00724 <jm00724@cs6261-vm03.uwg.westga.edu>
2023-06-14 23:09:01 -0400

    started chapter 8

--- chapter6/template-driven/control-validity/src/app/app.component.ts
+++ chapter6/template-driven/control-validity/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Stock } from 'app/model/stock';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,6 @@ import { Stock } from 'app/model/stock';
 export class AppComponent implements OnInit {
   title = 'Stock Market App';
 
-  public stock: Stock;
-
   ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80, 'NYSE');
-  }
-
-  onToggleFavorite(stock: Stock) {
-    console.log('Favorite for stock ', stock, ' was triggered');
-    this.stock.favorite = !this.stock.favorite;
   }
 }
